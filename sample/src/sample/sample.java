d1a6ab4b3b64f989fe486b5517772d5b437c5502
Kyle Reiter <kylereiter@contrabizservices.com>
2014-07-23 14:55:32 -0500

    testing

---
+++ sample/src/sample/sample.java
@@ -0,0 +1,20 @@
+package sample;
+
+/**
+ * Copyright 2014 Contra Software
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @author kylereiter
+ *         Jul 23, 2014 2:40:08 PM
+ */
+
+public class sample
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!!");
+    }
+}
